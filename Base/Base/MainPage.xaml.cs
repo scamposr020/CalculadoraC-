8ddcc114291d45934f16bf3d0da98941cd8515f1
scamposr020 <scamposr020@ulacit.ed.cr>
2020-11-08 14:15:09 -0600

    Inicializar

---
+++ Base/Base/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Base
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btn.Clicked += Btn_Clicked;
+            ss.Maximum = 360;    
+        }
+
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            lbl1.Text = dtp.Date.ToString()+ " " + ss.Value.ToString() + "" + txt1.Text;
+            txt1.IsEnabled = false;
+        }
+    }
+}
